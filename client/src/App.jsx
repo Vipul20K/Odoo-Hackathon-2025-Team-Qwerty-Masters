bc81b10043763e00c1f28790eb044cceac4de122
Vipul20K <thevkr20@gmail.com>
2025-07-12 12:37:18 +0530

    WIP: saving local changes before merging upstream

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
+import SignUpForm from "./components/SignUpForm"
+import LoginForm from "./components/LoginForm"
+import ProfilePage from "./components/ProfilePage"
+import AddQuestion from "./components/AddQuestion"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
-    <div>
-      App
-    </div>
+  <Router>
+         <Routes>
+           <Route path="/signup" element={<SignUpForm />} />
+           <Route path="/login" element={<LoginForm />} />
+           <Route path="/profile" element={<ProfilePage />} />
+           <Route path="/add-question" element={<AddQuestion/>} />
+          
+         </Routes>
+
+   </Router>
   )
 }
 
-export default App
+export default App;
